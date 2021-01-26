bb66852e4d116c1129702421cfbde6edc482561d
Lori Lynn <72951081+loriamarin@users.noreply.github.com>
2021-01-25 23:08:35 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalButton = document.querySelectorAll('[data-equals]')
